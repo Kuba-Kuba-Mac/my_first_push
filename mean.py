fc90c31fde5b51d77a7f56bbb6ff5aa69b4b0dfd
kuba <kubanychbeknasipbekov354@gmail.com>
2026-03-27 18:57:01 +0600

    first commit

---
+++ mean.py
@@ -0,0 +1,15 @@
+operator = input("Choose your operation (* + - /) : ")
+num1 = float(input("Enter your first number :"))
+num2 = float(input("Enter your second number :"))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator == "*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+else :
+    print(f"{operator} is not valid")
+
