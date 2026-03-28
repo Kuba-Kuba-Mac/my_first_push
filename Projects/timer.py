19ca9a5ddfb96c243203ffbaa6ab4da66c72d118
kuba <kubanychbeknasipbekov354@gmail.com>
2026-03-28 21:23:06 +0600

    i just know how to build timer or alarm

---
+++ Projects/timer.py
@@ -0,0 +1,11 @@
+import time
+
+n = int(input("Enter the time n second :"))
+for x in range(n,0,-1):
+    second = x % 60
+    minutes = int((x / 60) % 60)
+    hour = int(x/3600) % 24
+    print(f"{hour:02}:{minutes:02}:{second:02}")
+    time.sleep(1)
+
+print("Time's up nigger ")
---
+++ learning/for_loop.py
@@ -0,0 +1,31 @@
+# learning for loop
+
+
+# credit_card = "1234-5678-9012-3456"
+# for i in credit_card:
+#     print(i)
+
+
+# for i in range(1, 20):
+#     if i == 13:
+#         break
+#     else :print(i)
+
+
+# nested loop inner and outer loops
+#                 outer loop
+#                     inner loop
+
+# for i in range(3) :
+#     for x in range(1,10):
+#         print(x,end=" ")
+#     print(end="\n")
+
+
+# col = int(input("Enter the # of columns :"))
+# row = int(input("Enter the # of rows :"))
+# s = input("Enter what symbol will be in rectangle :")
+# for i in range(col) :
+#     for x in range(row):
+#         print(s,end=" ")
+#     print(end="\n")
