f3c913a0a08b9d9589e12b671c97decf37ae1cd4
kuba <kubanychbeknasipbekov354@gmail.com>
2026-03-28 10:35:11 +0600

    i just update my files

--- solve.py
+++ solve.py
@@ -1,1 +1,13 @@
+weight  = float(input("Enter your weight :"))
+unit = input("Kilogram or unit (K / P): ")
 
+if unit == 'k':
+    weight = weight * 2.205
+    unit = "Lbs."
+elif unit =='p':
+    weight = weight / 2.205
+    unit = "Kilo."
+else :
+    print(f"{unit} was not valid")
+
+print(f"Your weight is {round(weight,2)} {unit} ")
