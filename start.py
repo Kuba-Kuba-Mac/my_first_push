bacf362831a84396f81841f488fa2bd9e2bd28be
nas1pbek0v <kubanychbeknasipbekov354@gmail.com>
2026-03-28 12:34:51 +0600

    Update start.py

--- start.py
+++ start.py
@@ -1,26 +1,16 @@
-# # weight project
-# weight  = float(input("Enter your weight :"))
-# unit = input("Kilogram or unit (K / P): ")
-#
-# if unit == 'k':
-#     weight = weight * 2.205
-#     unit = "Lbs."
-# elif unit =='p':
-#     weight = weight / 2.205
-#     unit = "Kilo."
-# else :
-#     print(f"{unit} was not valid")
-#
-# print(f"Your weight is {round(weight,2)} {unit} ")
+# weight project
+weight  = float(input("Enter your weight :"))
+unit = input("Kilogram or unit (K / P): ")
+
+if unit == 'k':
+    weight = weight * 2.205
+    unit = "Lbs."
+elif unit =='p':
+    weight = weight / 2.205
+    unit = "Kilo."
+else :
+    print(f"{unit} was not valid")
+
+print(f"Your weight is {round(weight,2)} {unit} ")
 
 
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-    "model": "Mustang",
-    "brand": "Ford",
-  "model": "Mustang",
-    "model": "Mustang",
-  "year": 1964
-}
-print(thisdict)
