414c0ae17fcd79b5c85b60cd401bf790f2fc40bd
kuba <kubanychbeknasipbekov354@gmail.com>
2026-06-26 21:10:00 +0600

    start some project with OOP

---
+++ learning/OOP/Basketbal_prokect.py
@@ -0,0 +1,14 @@
+class Player():
+    def __init__(self,name,score,assists,rebound):
+        self.name = name
+        self.score = score
+        self.assists = assists
+        self.rebound = rebound
+        self.players = []
+
+
+    def add_player(self,name,score,assists,rebound):
+        self.players.append()
+
+
+
--- returning_projects/basketball_project /main.py
+++ returning_projects/basketball_project /main.py
@@ -1,1 +1,26 @@
 # basketball player states but with OOP
+class Player():
+    def __init__(self, name, score, assists, rebound):
+        self.name = name
+        self.score = score
+        self.assists = assists
+        self.rebound = rebound
+        self.players = []
+
+    def add_player(self, name, score, assists, rebound):
+        self.players.append({
+        "name": name,
+        "score": score,
+        "assists": assists,
+        "rebound": rebound
+        })
+
+    def show_all(self):
+        for i,x in enumerate(players,start=1):
+
+
+
+
+s = Player("lebron",12,12,12,)
+s.add_player("kuba",30,12,12)
+
